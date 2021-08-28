14a73ed9df96cef8632435313df33fb5622f0d09
Vitaliy Filippov <vitalif@yourcmc.ru>
2021-08-28 16:34:45 +0300

    Add a test

---
+++ __tests__/index-test.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const babel = require('@babel/core');
+const jsx = require('@babel/plugin-transform-react-jsx');
+const plugin = require('../');
+
+var example = '\
+var name = "John";\
+var foo = "Ё-Mobile";\
+foo = "Меня зовут "+name+"!  ";\
+foo = `  Меня зовут ${name}!`;\
+foo = <span>Привет!</span>;\
+foo = L("У меня много {1}: ", L("братьев"));\
+';
+
+it('works', () =>
+{
+    const { code } = babel.transform(example, { filename: 'test', plugins: [ jsx, plugin ] });
+    expect(code).toMatchSnapshot();
+    expect(JSON.parse(fs.readFileSync('react-translate-output.json'))).toEqual({
+        test: [
+            "Ё-Mobile",
+            "Меня зовут {1}!",
+            "Привет!",
+            "У меня много {1}: ",
+            "братьев",
+        ]
+    });
+});
